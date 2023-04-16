f7f7c4dc37c242cba3444c8ca61937454bb9385e
vikhyathkiran23 <72803191+vikhyathkiran23@users.noreply.github.com>
2023-04-16 12:49:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    canvas = createCanvas(550, 550)
+    canvas.position(560, 150);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("Posenet Initialised");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist + " scoreRightWrist = " + scoreRightWrist);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        console.log("Left Wrist X= " + leftWristX);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        console.log("Right Wrist X= " + rightWristX);
+    }
+}
